587745300fc8fb772d010fed6993881ab13e59a4
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2022-06-19 10:39:04 +0800

    ✨ Use csv file to store data

--- spider.py
+++ spider.py
@@ -1,5 +1,6 @@
 import requests
 import re
+import csv
 from bs4 import BeautifulSoup
 
 headers = {
@@ -8,13 +9,38 @@ headers = {
 
 base_url = "https://www.mcbbs.net/"
 
+try:
+    csvfile = open("data.csv","r",newline='',encoding="utf-8-sig")
+except FileNotFoundError:
+    csvfile = open("data.csv","w",newline='',encoding="utf-8-sig")
+reader = csv.reader(csvfile)
+
+thread_id_old = [row[0] for row in reader]
+data = {}
+
 r = requests.get("https://www.mcbbs.net/forum-news-1.html", headers=headers)
 bs = BeautifulSoup(r.text, "html.parser")
 list = bs.findAll("tbody", id=re.compile("normalthread_*"))
 
 for i in list:
     thread_id = re.compile(r"\d+").search(i["id"]).group()
-    type = i.tr.th.em.text
+    type = i.tr.th.em.text.strip("[]")
     title = i.tr.th.find("a", class_="s xst").text
     url = base_url + i.tr.th.find("a", class_="s xst")["href"]
-    print(thread_id + "> " + type + " " + title + "\n" + url)
+    #writer.writerow([thread_id,type,title,url])
+    #print(thread_id + "> " + type + " " + title + "\n" + url)
+    data[thread_id]=[type,title,url]
+
+data_new = []
+for key in data.keys():
+    if key in thread_id_old: break
+    data_new.append(key)
+
+if data_new != []:
+    csvfile = open("data.csv","a",newline='',encoding="utf-8-sig")
+    writer = csv.writer(csvfile)
+    data_new.reverse() 
+    for i in data_new:
+        writer.writerow([i,data[i]])
+
+csvfile.close()
