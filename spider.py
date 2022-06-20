d9f117385b72ba067313d64a71ebd732ffd0e1db
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2022-06-20 22:22:21 +0800

    🎨 Improve code structure

--- spider.py
+++ spider.py
@@ -6,38 +6,48 @@ from bs4 import BeautifulSoup
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36"
 }
-
 base_url = "https://www.mcbbs.net/"
 
-try:
-    csvfile = open("data.csv","r+",newline='',encoding="utf-8")
-except FileNotFoundError:
-    csvfile = open("data.csv","w+",newline='',encoding="utf-8")
-reader = csv.reader(csvfile)
 
-thread_id_old = [row[0] for row in reader]
-data = {}
+def get_data():
+    data = {}
+    r = requests.get(base_url + "forum-news-1.html", headers=headers)
+    bs = BeautifulSoup(r.text, "html.parser")
+    list = bs.findAll("tbody", id=re.compile("normalthread_*"))
 
-r = requests.get("https://www.mcbbs.net/forum-news-1.html", headers=headers)
-bs = BeautifulSoup(r.text, "html.parser")
-list = bs.findAll("tbody", id=re.compile("normalthread_*"))
+    for i in list:
+        thread_id = re.compile(r"\d+").search(i["id"]).group()
+        type = i.tr.th.em.text.strip("[]")
+        title = i.tr.th.find("a", class_="s xst").text
+        url = base_url + i.tr.th.find("a", class_="s xst")["href"]
+        data[thread_id] = [type, title, url]
+    return data
 
-for i in list:
-    thread_id = re.compile(r"\d+").search(i["id"]).group()
-    type = i.tr.th.em.text.strip("[]")
-    title = i.tr.th.find("a", class_="s xst").text
-    url = base_url + i.tr.th.find("a", class_="s xst")["href"]
-    data[thread_id]=[type,title,url]
 
-data_new = []
-for key in data.keys():
-    if key in thread_id_old: break
-    data_new.append(key)
+def main():
+    try:
+        csvfile = open("data.csv", "r+", newline="", encoding="utf-8")
+    except FileNotFoundError:
+        csvfile = open("data.csv", "w+", newline="", encoding="utf-8")
+    reader = csv.reader(csvfile)
+    
+    thread_id_old = [row[0] for row in reader]
+    data = get_data()
+    data_new = []
+    
+    for thread_id in data.keys():
+        if thread_id in thread_id_old:
+            break
+        data_new.append(thread_id)
 
-if data_new != []:
-    writer = csv.writer(csvfile)
-    data_new.reverse() 
-    for i in data_new:
-        writer.writerow([i]+data[i])
+    if data_new != []:
+        writer = csv.writer(csvfile)
+        data_new.reverse()
+        for i in data_new:
+            writer.writerow([i] + data[i])
 
-csvfile.close()
+    csvfile.close()
+
+
+if __name__ == "__main__":
+    main()
